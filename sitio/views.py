c93ee095827de843f1b4fd2a654e1032aeadf519
Francisco Jordan <jupiter@Mac-mini-de-Francisco.local>
2019-02-27 23:51:05 -0300

    optimizacion logica para obtener tipo de operacion

--- sitio/views.py
+++ sitio/views.py
@@ -17,6 +17,19 @@ def tmc_view(request):
         cuotas = int(request.POST['cuotas'])
         month = request.POST['fecha'][3:5]
         year = request.POST['fecha'][6:]
+
+        rangos_lte3 = {('11', '21'): [0, 5000],
+                       ('10', '26'): [5001, float('inf')]}
+
+        rangos_gt3 = {('45',): [0, 50],
+                      ('4', '28'): [0, 100],
+                      ('7', '30', '33'): [0, 200],
+                      ('44',): [51, 200],
+                      ('5', '31'): [101, 200],
+                      ('8', '27', '35'): [201, 5000],
+                      ('6', '32'): [201, float('inf')],
+                      ('9', '29', '34'): [5001, float('inf')]}
+
         if reajustable:
             if cuotas > 12:
                 tipo = '20', '23'
@@ -27,27 +40,13 @@ def tmc_view(request):
             else:
                 tipo = '12', '21'
         elif cuotas <= 3:
-            if monto > 5000:
-                tipo += '11', '25'
-            else:
-                tipo += '10', '26'
+            tipo = [tipos for tipos, (low, high) in rangos_lte3.items()
+                    if low <= monto <= high]
+            tipo = [i for sub in tipo for i in sub]
         else:
-            if monto <= 200:
-                tipo += '7', '30', '33'
-                if monto <= 100:
-                    tipo += '4', '28'
-                elif monto in range(101, 201):
-                    tipo += '5', '31'
-            else:
-                tipo += '6', '32'
-            if monto in range(0, 51):
-                tipo += '45',
-            elif monto in range(51, 201):
-                tipo += '44',
-            elif monto in range(201, 5001):
-                tipo += '8', '27', '35'
-            elif monto > 5000:
-                tipo += '9', '29', '34'
+            tipo = [tipos for tipos, (low, high) in rangos_gt3.items()
+                    if low <= monto <= high]
+            tipo = [i for sub in tipo for i in sub]
 
         if tipo:
             try:
